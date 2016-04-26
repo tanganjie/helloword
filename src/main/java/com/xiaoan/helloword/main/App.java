cb9e39c051474cbc766d16d1eed8608ecce15cce
tanganjie <t_anjie@qq.com>
2016-04-27 01:01:32 +0800

    init

---
+++ src/main/java/com/xiaoan/helloword/main/App.java
@@ -0,0 +1,15 @@
+package com.xiaoan.helloword.main;
+
+/**
+ * Created by tanganjie on 2016/4/27.
+ */
+public class App {
+
+    public static void print() {
+        System.out.println("helloWord");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("helloWord");
+    }
+}
---
+++ src/test/java/com/xiaoan/helloword/main/test/AppHelloword.java
@@ -0,0 +1,15 @@
+package com.xiaoan.helloword.main.test;
+
+import com.xiaoan.helloword.main.App;
+import org.junit.Test;
+
+/**
+ * Created by tanganjie on 2016/4/27.
+ */
+public class AppHelloword {
+
+    @Test
+    public void helloword() {
+        App.print();
+    }
+}
